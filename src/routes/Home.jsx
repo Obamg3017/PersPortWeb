889b7aca03562b20ade5e255ce18e228e0b9ad82
Olorunfemi <Femibam1@gmail.com>
2023-10-18 22:42:50 -0400

    mobileResponseHomeScreenUpdate

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -10,19 +10,19 @@ const Home = () => {
             Olorunfemi Bamgbose
           </h2>
           <h3 className="text-xl sm:text-2xl font-bold my-7 text-center ">
-            Fullstack Developer + Senior Underwriter
+            Fullstack Developer + Underwriter
           </h3>
           
-          <p className="text-xs font-semibold sm:text-lg text-center my-11">
-            I am a firm believer in having a solution base approach in almost
+          <p className="text-sm sm:text-lg text-center my-11">
+            I am a firm believer in having a solution based approach in almost
             everything that I do-as a senior underwriter for the past ten years,
             gathering information, evaluating risks, making decisions, and
             generating policies base on those factors, eventually led to
             policyholders being affected by the underwriting process-in turn
-            this often-created difficult discourses, which almost always
-            prompted me to put on my “solution base” hat and find solutions that
+            this often created difficult discourses, which almost always
+            prompted me to put on my “solution focused” hat and find alternatives that
             better served policyholders. Now as a fullstack developer, my
-            “solution base” hat stays on around the clock, as I am a passionate
+            “solution focused” hat stays on around the clock, as I am a passionate
             believer in solutions through software development. With my
             experience as a underwriter, adaptability, and joy of learning new
             languages, tools, processes, I am avidly seeking new challenges to
