0bada580a8474f677dd354d22fcdef434648685b
Olorunfemi <Femibam1@gmail.com>
2023-10-18 23:19:34 -0400

    updateDescription

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -12,22 +12,10 @@ const Home = () => {
           <h3 className="text-xl sm:text-2xl font-bold my-7 text-center ">
             Fullstack Developer + Underwriter
           </h3>
-          
-          <p className="text-sm sm:text-lg text-center my-11">
-            I am a firm believer in having a solution based approach in almost
-            everything that I do-as a senior underwriter for the past ten years,
-            gathering information, evaluating risks, making decisions, and
-            generating policies base on those factors, eventually led to
-            policyholders being affected by the underwriting process-in turn
-            this often created difficult discourses, which almost always
-            prompted me to put on my “solution focused” hat and find alternatives that
-            better served policyholders. Now as a fullstack developer, my
-            “solution focused” hat stays on around the clock, as I am a passionate
-            believer in solutions through software development. With my
-            experience as a underwriter, adaptability, and joy of learning new
-            languages, tools, processes, I am avidly seeking new challenges to
-            align with my interests and leverage my passions. If you want to get
-            to know me better, please feel free to explore my
+
+          <p className="text-sm font-bold sm:text-lg text-center my-11">
+            With a decade of experience as an underwriter, I approach
+            challenges with a solution-oriented mindset. My background involves assessing risks, quality control, decision making, and necessitating creative problem-solving. Now as a dedicated fullstack developer, my "solution-focused" hat stays on around the clock and I bring my expertise to crafting software solutions. Eager for new challenges, I combine adaptability and passion to create impactful outcomes. If you want to get to know me better, please feel free to explore my   
             {/* <a
               href="./about"
               // target="_blank"
@@ -37,7 +25,7 @@ const Home = () => {
               about
             </a> */}
             <Link to="/about" className="text-yellow-600 mx-2">
-              about
+              ABOUT
             </Link>
             page!
           </p>
